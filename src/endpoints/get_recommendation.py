fc877ae5db677daa08a54de763f6d54709b1935c
harrisonstark <harrisonstarkshs@gmail.com>
2023-09-30 02:47:53 -0400

    cringe recommendation changes

--- src/endpoints/get_recommendation.py
+++ src/endpoints/get_recommendation.py
@@ -1,7 +1,10 @@
 from fastapi import APIRouter, Request
 from src.utils.logger import configure_logging
 from src.utils.utils import add_query_params_to_url, get_user_info, retrieve_tokens
+import urllib.parse
 import httpx
+import numpy as np
+import json
 
 # Set up custom logger for error logging
 log = configure_logging()
@@ -18,26 +21,46 @@ async def get_recommendation(request: Request):
 
     access_token = retrieve_tokens(user_id, email)["access_token"]
 
-    keys_to_exclude = ['user_id', 'email', 'message']
+    message = query_params.get('message', None)
 
-    new_query_params = {key: value for key, value in query_params.items() if key not in keys_to_exclude}
+    base_url = "https://api.spotify.com/v1/recommendations?limit=5"
 
-    message = query_params['message']
-    # seed_genres = request to chatgpt with message
+    values = ["acousticness", "danceability", "energy", "instrumentalness", "liveness", "speechiness", "valence"]
 
+    if(message):
+        # TODO: seed_genres = chatgpt response, add query params seed_genres
+        seed_genres = {"seed_genres": "indie-pop"}
+        base_url = add_query_params_to_url(base_url, seed_genres)
+        base_url = add_query_params_to_url(base_url, {"target_" + str(value): 0.5 for value in values})
+    else:
+        track_list = query_params['track_list']
+        log.error(track_list)
+        track_list = urllib.parse.unquote(track_list)
+        log.error(track_list)
+        track_list = json.loads(track_list)
+        # Extract values for calculation
+        attribute_values = {
+            attribute: [v[attribute] for v in track_list.values()] for attribute in track_list["0"].keys() if attribute != "time_listened"
+        }
+        
+        output_values = {"target_" + attribute: np.mean(0.5 + values * np.square([v["time_listened"] for v in track_list.values()]) - 0.5 * np.square([v["time_listened"] for v in track_list.values()]))
+               for attribute, values in attribute_values.items()}
+        seed_genres = query_params["seed_genres"]
+        seed_genres = urllib.parse.unquote(seed_genres)
+        seed_genres = {"seed_genres": seed_genres}
+        base_url = add_query_params_to_url(base_url, seed_genres)
+        base_url = add_query_params_to_url(base_url, output_values)
+        log.error(output_values)
     headers = {
         "Authorization": f"Bearer {access_token}",
         "Content-Type": "application/x-www-form-urlencoded",
     }
 
-    # Base URL
-    # Convert JSON data to query parameters
-    base_url = add_query_params_to_url("https://api.spotify.com/v1/recommendations", new_query_params)
     try:
         async with httpx.AsyncClient() as client:
             response = await client.get(base_url, headers=headers)
         data = response.json()
-        return [track["uri"] for track in data["tracks"]]
+        return {"songs": [track["uri"] for track in data["tracks"]], "seed_genres": seed_genres["seed_genres"]}
     except Exception as e:
         # Log the error message using the custom logger
         error_message = e
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -16,11 +16,13 @@ def json_to_query_params(json_data):
         return ""
 
     query_params = urllib.parse.urlencode(json_data)
-    return f"?{query_params}"
+    return f"{query_params}"
 
 def add_query_params_to_url(base_url, json_data):
     query_params = json_to_query_params(json_data)
-    return f"{base_url}{query_params}"
+    if("?" not in base_url):
+        return f"{base_url}?{query_params}"
+    return f"{base_url}&{query_params}"
 
 def find_user(user_id, email):
     db = Database()
