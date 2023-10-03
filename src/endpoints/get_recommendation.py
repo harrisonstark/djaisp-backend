ef74cc78dc0111c7f1911f0c7ba54ef47fe57259
harrisonstark <harrisonstarkshs@gmail.com>
2023-10-03 14:58:18 -0400

    clean up code

--- src/endpoints/get_recommendation.py
+++ src/endpoints/get_recommendation.py
@@ -45,30 +45,28 @@ async def get_recommendation(request: Request):
         track_list = query_params['track_list']
         track_list = urllib.parse.unquote(track_list)
         track_list = json.loads(track_list)
+
         # Extract "thumbs" values
         thumbs_values = [v["thumbs"] for v in track_list.values()]
-        log.error(thumbs_values)
+
         # Create arrays for "thumbs" equal to "up", "down", and ""
         up_array = [v for v, thumbs in zip(track_list.values(), thumbs_values) if thumbs == "up"]
-        log.error(up_array)
         down_array = [v for v, thumbs in zip(track_list.values(), thumbs_values) if thumbs == "down"]
-        log.error(down_array)
         empty_array = [v for v, thumbs in zip(track_list.values(), thumbs_values) if thumbs == ""]
-        log.error(empty_array)
+
         modified_down_array = [{attribute: 1 - value if attribute != "thumbs" else value for attribute, value in entry.items()} for entry in down_array]
-        log.error(modified_down_array)
+
         new_track_list_np = np.concatenate([up_array, up_array, modified_down_array, empty_array])
-        log.error(new_track_list_np)
+
         # Extract values for calculation
         attribute_values = {
             attribute: [v[attribute] for v in new_track_list_np] for attribute in track_list["0"].keys() if attribute != "time_listened" and attribute != "thumbs"
         }
-        log.error(attribute_values)
         
         output_values = {"target_" + attribute: np.mean(0.5 + values * np.square([track[attribute] for track in new_track_list_np]) - 
             0.5 * np.square([track[attribute] for track in new_track_list_np]))
             for attribute, values in attribute_values.items()}
-        log.error(output_values)
+        
         seed_genres = query_params["seed_genres"]
         seed_genres = urllib.parse.unquote(seed_genres)
         base_url = add_query_params_to_url(base_url, {"seed_genres": seed_genres})
